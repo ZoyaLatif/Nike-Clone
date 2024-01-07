60b7883ce30b969c4bea3eec6ab3f05e76366abd
ZoyaLatif <124717752+ZoyaLatif@users.noreply.github.com>
2024-01-07 17:07:15 +0000

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# Nike-Clone
+# React + Vite
 
-In this Project, you'll:
-- Learn how to make the most out of Tailwind CSS
-- Fully understand how Tailwind works behind-the-scenes
-- Follow all of Tailwind's best practices
-- Put all of your newly gained Tailwind skills to the test by building a beautiful Nike landing page
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+
+Currently, two official plugins are available:
+
+- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
+- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
---
+++ src/sections/SpecialOffer.jsx
@@ -0,0 +1,42 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import { Button } from "../components";
+
+const SpecialOffer = () => {
+  return (
+    <section className='flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container'>
+      <div className='flex-1'>
+        <img
+          src={offer}
+          alt='Shoe Promotion'
+          width={773}
+          height={687}
+          className='object-contain w-full'
+        />
+      </div>
+      <div className='flex flex-1 flex-col'>
+        <h2 className='text-4xl font-palanquin font-bold'>
+          <span className='text-coral-red'>Special </span>
+          Offer
+        </h2>
+        <p className='mt-4 info-text'>
+        Set out on a shopping trip with incredible bargains that will transform your experience. We stand out for our exceptional value, which includes both outstanding selections and amazing savings.
+        </p>
+        <p className='mt-6 info-text'>
+        Explore a world of opportunities created to exceed even your highest hopes and satisfy your particular needs. Your experience with us has been nothing short of extraordinary.
+        </p>
+        <div className='mt-11 flex flex-wrap gap-4'>
+          <Button label='Shop now' iconURL={arrowRight} />
+          <Button
+            label='Learn more'
+            backgroundColor='bg-white'
+            borderColor='border-slate-gray'
+            textColor='text-slate-gray'
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SpecialOffer;
